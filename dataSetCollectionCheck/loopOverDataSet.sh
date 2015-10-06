a6d68886f13a79465196fc394d83eb422d5b0f1a
Ravi Janjam <ravikishorejnjm@gmail.com>
2015-10-06 21:14:01 +0200

    updated the script loopOverDataSet.sh

--- dataSetCollectionCheck/loopOverDataSet.sh
+++ dataSetCollectionCheck/loopOverDataSet.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+
 filename=$1
+collectionName=$2
+
 IFS=$'\n'
 
-if [ $# -ne 1 ]
+if [ $# -ne 2 ]
 then
 
 	echo "Insufficient number of arguments provided "
-	echo "to run the script do loopOverAllFiles yourfile.txt"
+	echo " "; echo " "; 
+	echo "to run the script do loopOverAllFiles <yourfile> <Track>"
+	
+	echo "Track is the collection you want to look for in the dataset"
 	exit 1
 fi
 
@@ -19,7 +25,7 @@ do
 	echo "file count: $counter"
 	echo "now running edmDumpEvent Content for file : ${next}"
 # 	edmDumpEventContent root://xrootd-cms.infn.it/${next} | grep -i TrackCollection
- 	edmDumpEventContent ${next} | grep -i TrackCollection
+ 	edmDumpEventContent ${next} | grep -i ${collectionName}
 	echo " "; echo " "; 
 
 	
