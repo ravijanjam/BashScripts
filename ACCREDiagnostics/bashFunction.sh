7e19a0d051065028bd670cd6ac938c98c0f13c73
Ravi Janjam <ravi.k.janjam@vanderbilt.edu>
2015-11-17 07:43:34 -0600

    function example

--- ACCREDiagnostics/bashFunction.sh
+++ ACCREDiagnostics/bashFunction.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+echo "This program enables and disables a function"
+
+
 testFn(){
 
+	echo "========================="
 	echo "From inside test function"
+	echo "========================="
+
+	echo "First argument is " $1
+	echo "Second argument is " $2
+	echo "Number of arguments is " $#
 }
 
 
 # Calling up function
-testFn
+testFn 2 3
 
 # Disable the function, unsets permanently, can't be called back
 unset testFn
