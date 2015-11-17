4dbf5ba1fa8104a626d2394bcf800a82d7ef76c1
Ravi Janjam <ravi.k.janjam@vanderbilt.edu>
2015-11-17 09:22:38 -0600

    cms jobs to the node map to get memory usage stats

---
+++ ACCREDiagnostics/cmsJobsToNodeMap.sh
@@ -0,0 +1,24 @@
+
+#!/bin/bash
+
+echo "The goal of this script is to get the memory information on the node in which cms jobs are running"
+
+nCMSJobs=`sacct -a | grep -w cms |wc -l`
+nCMSRunningJobs=`sacct -a | grep -w cms | grep -w  RUNNING |wc -l`
+nCMSPendingJobs=`sacct -a | grep -w cms | grep -w  PENDING |wc -l`
+
+echo "The number of CMS jobs as of the moment" ${nCMSJobs}
+echo "The number of Running CMS jobs as of the moment" ${nCMSRunningJobs}
+echo "The number of Pending CMS jobs as of the moment" ${nCMSPendingJobs}
+
+
+<<comment
+clear;
+query=`sacct -a | grep -w cms | awk '{print $i}'`
+for i in $query;
+do
+	echo "=========================="
+	echo -ne `scontrol show job $i`;
+	echo "=========================="
+done
+comment
