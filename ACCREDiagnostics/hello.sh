467687fc9268d7a59935d552754037af436ab6d8
Ravi Janjam <ravi.k.janjam@vanderbilt.edu>
2016-03-15 18:53:54 -0500

    remove some junk

--- ACCREDiagnostics/hello.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Hello, from gateway $1"
-echo "Whats up"
--- ACCREDiagnostics/simple.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-info=$(hostname; date)
-
-for i in `cat /usr/local/etc/x86.nodes`; do rsh ${i} echo "from" $info >> /home/janjamrk/ACCREDiagnostics/crontest.out; done
-
-
