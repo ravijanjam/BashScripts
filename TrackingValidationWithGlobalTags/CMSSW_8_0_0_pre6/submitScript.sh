d1c04dd7d31bd7fbc758b45b35a6df3f3e5563f8
ravijanjam <ravikishorejnjm@gmail.com>
2016-04-04 17:42:36 +0200

    udpated with sh

---
+++ TrackingValidationWithGlobalTags/CMSSW_8_0_0_pre6/submitScript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+filePath=/afs/cern.ch/user/r/rjanjam/TrackStuff/CMSSW_8_0_0_pre6/src/140.53_RunHI2011+RunHI2011+RECOHID11+HARVESTDHI
+$filePath/runJobs.sh  80X_dataRun2_v8
+$filePath/runJobs.sh  80X_dataRun2_v0
+#$filePath/runJobs.sh 76X_dataRun1_v10
